7e4a062af4866c7a139a8d7f667db35f2eb5c855
ahmedasif-T2S <ahmed.asif@foodhub.com>
2025-07-03 00:12:49 +0530

    feat: retrofit without module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,18 +55,25 @@ dependencies {
     implementation(libs.androidx.material3)
     testImplementation(libs.junit)
 
-    implementation("com.google.dagger:hilt-android:2.56.2")
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-    kapt("com.google.dagger:hilt-android-compiler:2.56.2")
-    kapt("androidx.hilt:hilt-compiler:1.2.0")
+
 
     implementation("androidx.navigation:navigation-compose:2.9.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
+
+    // hilt dependencies
+    implementation("com.google.dagger:hilt-android:2.56.2")
     implementation("androidx.hilt:hilt-navigation-compose:1.3.0-alpha01")
-    
+    kapt("com.google.dagger:hilt-android-compiler:2.56.2")
+    kapt("androidx.hilt:hilt-compiler:1.2.0")
+
+    // Retrofit dependencies
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
--- app/src/main/java/com/example/giftcard/MyApp.kt
+++ app/src/main/java/com/example/giftcard/MyApp.kt
@@ -1,8 +1,47 @@
 package com.example.giftcard
 
 import android.app.Application
+import android.util.Log
+import com.example.giftcard.network.IProducts
+import com.google.gson.Gson
 import dagger.hilt.android.HiltAndroidApp
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 @HiltAndroidApp
-class MyApp: Application() {
+class MyApp : Application() {
+    lateinit var retrofit: Retrofit;
+    override fun onCreate() {
+        super.onCreate()
+        makeProductCallById(1)
+    }
+
+
+    private fun makeProductsCall() {
+        GlobalScope.launch {
+            retrofit =
+                Retrofit.Builder().baseUrl("https://fakestoreapi.com").addConverterFactory(
+                    GsonConverterFactory.create(Gson())
+                ).build()
+            val productService = retrofit.create(IProducts::class.java)
+            val response = productService.getProducts().execute()
+            val products = response.body()
+            Log.d("MainActivity", "Products: $products")
+        }
+    }
+
+    private fun makeProductCallById(id: Int) {
+        GlobalScope.launch {
+            retrofit =
+                Retrofit.Builder().baseUrl("https://fakestoreapi.com").addConverterFactory(
+                    GsonConverterFactory.create(Gson())
+                ).build()
+            val productService = retrofit.create(IProducts::class.java)
+            val response = productService.getProductById(id).execute()
+            val products = response.body()
+            Log.d("MainActivity", "Products By id: $products")
+        }
+    }
 }
